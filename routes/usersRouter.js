9fcd96858d45033350a0d0a0d6270e53da63d2cc
demchukv <demchukv@gmail.com>
2024-06-21 19:51:42 +0300

    upload avatars

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,8 +5,8 @@ import { upload } from "../helpers/upload.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/current", auth, ctrl.current);
 userRouter.get("/count-users", ctrl.countUsers);
+userRouter.get("/current", auth, ctrl.current);
 userRouter.patch("/avatar", auth, upload.single("avatar"), ctrl.updateAvatar);
 
 export default userRouter;
