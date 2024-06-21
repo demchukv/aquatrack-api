c6539d4d2e7db1c2528080f52d2fec7a9cc94cb9
demchukv <demchukv@gmail.com>
2024-06-21 16:04:05 +0300

    add route for current user

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+
+import { User } from "../models/user.js";
+
+export const current = async (req, res, next) => {
+  const { _id } = req.user;
+  try {
+    const user = await User.findUserById(_id);
+    res.json({
+      email: user.email,
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+      sex: user.sex,
+      weight: user.weight,
+      timeInSport: user.timeInSport,
+      waterPerDay: user.waterPerDay,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import auth from '../middlewares/authenticate.js';
+import * as ctrl from "../controllers/user.js";
 
 const userRouter = express.Router();
+
+userRouter.get("/current", auth, ctrl.current);
 
 export default userRouter;
