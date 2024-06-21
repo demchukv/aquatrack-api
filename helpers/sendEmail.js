75738ce4f901ad5679b05277275f3ef764469f7f
demchukv <demchukv@gmail.com>
2024-06-21 23:48:18 +0300

    email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -4,8 +4,6 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail = async (data) => {
   const msg = { ...data, from: "demchuk.volodymyr@lnu.edu.ua" };
-  console.log("try send email");
   await sgMail.send(msg);
-  console.log("Email sended");
   return true;
 };
