bfbe817326a02e49f64970e8ada5900277d0034a
demchukv <demchukv@gmail.com>
2024-07-02 18:00:14 +0300

    check

--- controllers/auth.js
+++ controllers/auth.js
@@ -113,10 +113,10 @@ const verifyEmail = async (req, res, next) => {
       verify: true,
       verificationToken: null,
     });
-    res.json({
-      message: 'Verification successful',
-    }); 
-    // return res.redirect(`${process.env.FRONTEND_URL}/signin?email-verified`);
+    // res.json({
+    //   message: 'Verification successful',
+    // }); 
+    return res.redirect(`${process.env.FRONTEND_URL}/signin?email-verified`);
 
   } catch (error) {
     console.error(error);
